4c20f2e23c8408d6fbfb56fbd29f0f9e46e5255b
Hiago Oliveira <99771272+hiag0liveira@users.noreply.github.com>
2023-09-25 23:55:39 -0300

    Delete src directory

--- src/entities/CorAparente.js
+++
@@ -1,9 +0,0 @@
-export default class CorAparente {
-  constructor() {
-    this.listaDeClassificacoes = ["boa", "adequada", "inadequada"];
-    this.boa = [-6, -3, 3, 6];
-    this.adequada = [3, 5, 15, 17];
-    this.inadequada = [13, 17, 30, 34];
-    this.unidade = "Unidade Hazen";
-  }
-}
